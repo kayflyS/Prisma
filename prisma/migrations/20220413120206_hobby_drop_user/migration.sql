9158c1ef32d948ee3c640bec9e65a48ac14220ff
Caleb <olupeniel2000@gmail.com>
2022-04-13 14:06:06 +0100

    learning prisma

---
+++ prisma/migrations/20220413120206_hobby_drop_user/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `hobby` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "hobby";
